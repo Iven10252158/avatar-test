e6d5f6824f1528e7684cc5963030314bd1078e70
Iven <iven@Ivens-MacBook-Pro.local>
2022-06-10 18:13:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+body, html {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  overflow: hidden;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <iframe class="avatar-test" src="https://room.metaspace.place/user/conf?id=7UQ4GRy0FTJcjXlxFeIQ" frameborder="0"></iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.avatar-test {
+  display: block;
+  width: 100%;
+  height: 100vh;
+}
+</style>
